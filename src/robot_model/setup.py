dde46964c0e38c0308e6b14af2c3cc8cf1c725a9
arnaucresp0 <arnau.crespo.santamaria@gmail.com>
2024-03-10 12:03:05 +0100

    Update package description and add file paths to setup.py

--- src/robot_model/setup.py
+++ src/robot_model/setup.py
@@ -1,4 +1,8 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+import os
+from glob import glob
+from setuptools import setup
+from setuptools import find_packages
 
 package_name = 'robot_model'
 
@@ -10,12 +14,15 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='pc',
     maintainer_email='arnau.crespo.santamaria@gmail.com',
-    description='TODO: Package description',
+    description='Show the quadruped robot model',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
